156f1d4493d915ab19f9808c4a66d63cfbee6bc3
Ghost Syncrt <gh0stsync.rt@gmail.com>
2016-01-26 16:13:12 +0700

    Create ReverseMsg.java

---
+++ ReverseMsg.java
@@ -0,0 +1,9 @@
+public class StringMessage {
+	public static void main(String[] args) {
+		String name = "ABCDEFG";
+		int i;
+		for(i=(name.length())-1;i>=0;i--){
+			System.out.print(name.charAt(i));
+			}
+		}
+}
