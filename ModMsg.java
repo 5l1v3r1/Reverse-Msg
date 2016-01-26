07bc05991965a4e9104bc8df98d3324a19692274
Ghost Syncrt <gh0stsync.rt@gmail.com>
2016-01-26 16:14:16 +0700

    Create ModMsg.java

---
+++ ModMsg.java
@@ -0,0 +1,17 @@
+public class StringMessageTest {
+	public static void main(String[] args) {
+		String name = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		int i;
+		for(i=0;i<=name.length()-1;i++){
+			if(i%2==0){
+			System.out.print(name.charAt(i));
+			}
+			
+		}
+		System.out.println();
+		
+		
+		
+	
+		}
+}
